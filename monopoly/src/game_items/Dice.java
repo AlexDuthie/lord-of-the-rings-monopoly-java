a34c5530e3b08033a369a08ae94cf16d0c98aaf1
Alex Duthie <alexduthiemain@outlook.com>
2021-02-25 00:24:38 +0000

    Dice object edit
    Player can now access each dice's results plus the combined result

--- monopoly/src/game_items/Dice.java
+++ monopoly/src/game_items/Dice.java
@@ -4,51 +4,23 @@ import java.util.Random;
 
 public class Dice {
 	
-	// function to roll two dice and store the result
-	public static int roll_dice() {
-		Random randomise = new Random();
-		
-		// there is two dice in monopoly
-		int dice_1_sides = 6;
-		int dice_2_sides = 6;
-		
-		// picks a random integer from the dice_1 and dice_2 values
-		// adds the two values together
-		int random_num = randomise.nextInt(dice_1_sides + dice_2_sides);
-		
-		// this should prevent a 0 being rolled
-		if(random_num == 0 || random_num == 1) {
-			random_num = randomise.nextInt(dice_1_sides + dice_2_sides);
-		}
-		
-		// returning the result of the two dice being rolled
-		return random_num;
-	}
+	Random randomise = new Random();
 	
+	// Dice Variables
+	int dice_sides = 6;
+	public int dice_1 = 0;
+	public int dice_2 = 0;
+	public int result = 0;
 	
-	// function to fix the grammar of the rolled dice result
-	// dice_num is passed through
-	public static String fix_dice_grammar(int dice_num) {
+	public int roll_dice() {
+		do {
+			dice_1 = randomise.nextInt(dice_sides);
+			dice_2 = randomise.nextInt(dice_sides);
+		} while(dice_1 == 0 || dice_2 == 0);
 		
-		String result = "";
-		
-		if(dice_num == 8 || dice_num == 11) {
-			result = "You have rolled an ";
-		} else {
-			result = "You have rolled a ";
-		}
+		result = dice_1 + dice_2;
 		
 		return result;
 	}
-	
-	// displays the result of the rolled dice to the player
-	public static void go() {
-		int dice_num = roll_dice();
-		
-		String fix_grammar = fix_dice_grammar(dice_num);
-		
-		System.out.println(fix_grammar + dice_num);
-	}
-
 	
 }
