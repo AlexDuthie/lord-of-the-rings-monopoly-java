01fc735e5063d13afa5c57e48cb20c09f7717941
Alex Duthie <alexduthiemain@outlook.com>
2021-02-25 22:28:55 +0000

    Loop to roll dice 3 times and set player position

--- monopoly/src/game_items/Board.java
+++ monopoly/src/game_items/Board.java
@@ -1,30 +1,38 @@
 package game_items;
 
 public class Board {
-
+	
 	public int player_spaces = 0;
 	public String board_location = "";
+	
+	// Reference: https://monopoly.fandom.com/wiki/List_of_Monopoly_Properties
 	public String[] board_locations = {
-			"Place1",
-			"Place2",
-			"Place3",
-			"Place4",
-			"Place5",
-			"Place6",
-			"Place7",
-			"Place8",
+			"Start",
+			"Place 1",
+			"Place 2",
+			"Place 3",
+			"Place 4",
+			"Place 5",
+			"Place 6",
 	};
 	
-	public void set_player_position(int player_moves) {
-		player_spaces += player_moves - 1;
-		board_location = board_locations[player_spaces];
-	}
-	
-	public int get_player_position() {
-		return player_spaces;
+	public void set_player_location(int player_moves) {
+		player_spaces += player_moves;
+		
+		if(player_moves >= board_locations.length) {
+			// 8, 
+			System.out.println("Full Circle!");
+			board_location = board_locations[player_moves - board_locations.length];
+		} else {
+			board_location = board_locations[player_moves];
+		}
 	}
 	
 	public String get_board_location() {
 		return board_location;
+	}
+	
+	public int get_player_spaces() {
+		return player_spaces;
 	}
 }
