6d862a8122e87d7ab881076f186d989598a09306
Alex Duthie <alexduthiemain@outlook.com>
2021-03-02 19:56:14 +0000

    Player spaces moved method unit tests

---
+++ monopoly/src/test/java/playerTests/PlayerSpacesMovedTest.java
@@ -0,0 +1,28 @@
+
+package playerTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game.Player;
+
+public class PlayerSpacesMovedTest {
+
+	@Test
+	public void test() {
+		// testing to see how many spaces a player has moved in total
+		
+		Player player = new Player();
+		
+		// loops 5 into player spaces 3 times to simulate a dice being thrown 3 times
+		for(int i = 0; i < 3; i++) {
+			player.set_player_spaces(5);
+		}
+		
+		int output = player.get_player_spaces();
+		
+		// the spaces should be 15 due to 5 being iterated 3 times
+		assertEquals(15, output);
+	}
+}
