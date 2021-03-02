ca77d8ab376b851c0bc9094ed73d9af7fb208fdb
Alex Duthie <alexduthiemain@outlook.com>
2021-03-02 19:55:34 +0000

    Player location method unit tests

---
+++ monopoly/src/test/java/playerTests/PlayerLocationTest.java
@@ -0,0 +1,37 @@
+package playerTests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import game.Player;
+
+public class PlayerLocationTest {
+
+	@Test
+	public void test() {
+		// board_locations can be seen below (15-21)
+		/*
+		 	public String[] board_locations = {
+			"Start",
+			"The Shire",
+			"Farmer Maggot's Crops",
+			...
+			...
+			};
+		 */
+		
+		Player player = new Player();
+		
+		// passing 2 spaces on the board through the parameters
+		// this would usually be done using a dice object
+		// the location should be the 2nd location in the board_locations array (Farmer Maggot's Crops)
+		player.set_player_location(2);
+		
+		String output = player.get_player_location();
+		
+		// tested manually several times
+		assertEquals("Farmer Maggot's Crops", output);
+	}
+
+}
