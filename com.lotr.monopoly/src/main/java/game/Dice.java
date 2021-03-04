c4b647cb90763b09364f6c3aed0ada79b985f12c
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-03-04 10:08:19 +0000

    Dice class: Setters/ Getters Added

--- com.lotr.monopoly/src/main/java/game/Dice.java
+++ com.lotr.monopoly/src/main/java/game/Dice.java
@@ -2,26 +2,59 @@ package game;
 
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Dice {
 	
+	public Scanner input = new Scanner(System.in);
+	
 	Random randomise = new Random();
 	
-	public int dice_1 = 0;
-	public int dice_2 = 0;
-	public int dice_result = 0;
+	public int dice1 = 0;
+	public int dice2 = 0;
+	public int diceResult = 0;
 	
-	public void roll_dice() {
+	// Dice 1
+	public void rollDice1() {
 		do {
-			dice_1 = randomise.nextInt(6);
-			dice_2 = randomise.nextInt(6);
-		} while(dice_1 == 0 || dice_2 == 0);
-		
-		dice_result = dice_1 + dice_2;
+			dice1 = randomise.nextInt(7);
+		} while(dice1 == 0);
 	}
 	
-	public int get_dice_result() {
-		return dice_result;
+	public int getRolledDice1() {
+		return dice1;
+	}
+	
+	// Dice 2
+	
+	public void rollDice2() {
+		do {
+			dice2 = randomise.nextInt(7);
+		} while(dice2 == 0);
+	}
+	
+	public int getRolledDice2() {
+		return dice2;
+	}
+	
+	// Roll Dice
+	
+	public int roll_dice() {
+		System.out.println("\nRoll Dice? (Yes or No)");
+		String answer = input.nextLine().toLowerCase();
+		
+		if(answer.equals("yes")) {
+			rollDice1();
+			rollDice2();
+		}
+		
+		diceResult = getRolledDice1() + getRolledDice2();
+		
+		return diceResult;
+	}
+	
+	public int getDiceResult() {
+		return diceResult;
 	}
 	
 }
