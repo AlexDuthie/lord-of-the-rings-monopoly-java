837660092537a4e7cbbdfb98edf88604936fcd74
ALEX DUTHIE <alexduthiemain@outlook.com>
2021-03-11 10:35:13 +0000

    JUnit Testing

---
+++ src/test/java/gameTests/AllGameTests.java
@@ -0,0 +1,11 @@
+package gameTests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ RollBothDiceTest.class, RollFirstDieTest.class, RollSecondDieTest.class })
+public class AllGameTests {
+
+}
